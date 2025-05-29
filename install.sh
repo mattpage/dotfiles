224360515c858cc10d5afddf2fa5b4b99af597b0
Matthew Page <mattpage@github.com>
2025-05-29 11:57:20 -0700

    install neovim as a package

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ PACKAGES_NEEDED="\
     ripgrep \
     fuse \
     universal-ctags \
+    neovim \
     yamllint"
 
 if ! dpkg -s ${PACKAGES_NEEDED} > /dev/null 2>&1; then
@@ -20,14 +21,9 @@ if ! dpkg -s ${PACKAGES_NEEDED} > /dev/null 2>&1; then
     sudo apt-get -y -q install ${PACKAGES_NEEDED}
 fi
 
-# install latest neovim
 sudo modprobe fuse
 sudo groupadd fuse
 sudo usermod -a -G fuse "$(whoami)"
-
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-sudo chmod u+x nvim.appimage
-sudo mv nvim.appimage /usr/local/bin/nvim
 
 ln -s $(pwd)/vimrc $HOME/.vimrc
 ln -s $(pwd)/vim $HOME/.vim
@@ -44,9 +40,10 @@ mkdir -p $(pwd)/vim/backup $(pwd)/vim/colors
 
 mkdir $HOME/vim-sessions
 
-vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
+# vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
 
 sudo chsh -s "$(which zsh)" "$(whoami)"
+source $HOME/.zshrc
 
 # some git settings
 git config --global init.defaultBranch main
